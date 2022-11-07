f7a99db56a8cac715975a145e81bbb321d1fe40f
rettigk2 <35699948+rettigk2@users.noreply.github.com>
2022-11-07 18:56:38 -0500

    Update Kyle Rettig Coordinate Lister.py

--- Kyle Rettig Coordinate Lister.py
+++ Kyle Rettig Coordinate Lister.py
@@ -1,67 +1,34 @@
 #Code by Kyle Rettig
-from math import acos, cos, sin, trunc, sqrt, floor
+from math import acos, cos, sin, trunc, sqrt, floor, asin
 from typing import final
+import matplotlib.pyplot as plt
 
 #Defining Arm Degree of Rotation
-s1 = [] #first arm degree of rotation
+A1 = [] #first arm degree of rotation
 for i  in range(0,91):
   if i%5==0: 
-        s1.append(i) 
+        A1.append(i*0.0174533) 
 
-s2 = [] #second arm degree of rotation
+A2 = [] #second arm degree of rotation
 for i  in range(9,91):
   if i%5==0: 
-        s2.append(i)
-
+        A2.append(i*0.0174533)
+        
 #Defining Lengths of Arms
-s1length = (10) #b first arm length
-s2length = (4.5) #c second arm length
+len1 = (10) #b first arm length
+len2 = (4.5) #c second arm length
 
-#Define all x,y coordinates of the first arm
-x1 = []
-y1 = []
-for i in range(0,len(s1)):
-    if s1length*cos(s1[i]) >= 0:
-        x1.append(s1length*cos(s1[i])) #10 value array
-    if s1length*sin(s1[i]) >= 0:
-        y1.append(s1length*sin(s1[i])) #8 value array
- 
-#Define all x,y coordinates for the second arm
-x2 = []
-y2 = []
-for i in range(0,len(s2)):
-    if s2length*cos(s2[i]) >= 0:
-        x2.append(s2length*cos(s2[i])) #8 value array
-    if s2length*sin(s2[i]) >= 0:
-        y2.append(s2length*sin(s2[i])) #7 value array
+x = []
+y = []
+for z in range(0,len(A1)):
+    for i in range(0,len(A2)):
+        hyp = sqrt((len1*len1)+(len2*len2)-(2*len1*len2*cos(A2[i])))
+        D2 = acos(((len1*len1)-(len2*len2)+(hyp*hyp))/(2*hyp*len1))
+        D1 = A1[z] - D2
+        x.append(hyp*cos(D1))
+        y.append(hyp*sin(D1))
+        print((x[i]) , (y[i]))
 
-
-#Combined final coordinate locations
-x3 = []
-y3 = []
-
-#Each array is a different max length, so theres a for loop for every size
-for i in range(0,len(x1)):
-    for s in range (0,len(x2)):
-        for k in range (0,len(y2)):
-            x3.append(x1[i]+x2[s])
-            y3.append(y1[s]+y2[k])
-
-
-#Remove Duplicates
-xt = []
-yt = [] 
-for i in x3: 
-    if i not in xt: 
-        xt.append(i)
-for i in y3: 
-    if i not in yt: 
-        yt.append(i) 
-xt.sort()
-yt.sort()
-
-#Print All Coordinates
-print("All Coordinate Pairs for 2 Sevro Arm:")
-for i in range(0, len(xt)):
-    for z in range(0, len(yt)):
-        print((xt[i],yt[z]))
+plt.plot(x,y)
+plt.show()
+        
