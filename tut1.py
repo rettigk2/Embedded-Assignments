5f05c4bffe458a34005e64a77d9e99e57464e2e1
rettigk2 <35699948+rettigk2@users.noreply.github.com>
2022-12-19 05:08:12 -0500

    Add files via upload

---
+++ tut1.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_restful import Api, Resource
+from datetime import datetime
+
+app = Flask(__name__)
+
+api = Api(app)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+class LogReadings(Resource):
+    def get(self, StationID, Sensor1, Sensor2, Sensor3, Verbose):
+        now = datetime.now()
+        with open('IoT_Data.txt', 'a') as f:
+            str1 = "%s\t %s\t %6.3f\t %6.3f\t %6.3f\n" %(StationID, now.strftime("%m/%d/%Y, %H:%M:%S"), Sensor1, Sensor2, Sensor3)
+            f.write(str1)
+        f.close()
+        if(Verbose ==1):
+            return{"StationID" : StationID, "Sensor1": Sensor1, "Sensor2": Sensor2, "Sensor3": Sensor3, "Date_Time": now.strftime("%m/%d/%Y, %H:%M:%S") }
+        else:
+            return{ "Logged @" : now.strftime("%m/%d/%Y, %H:%M:%S")}
+        
+
+api.add_resource(LogReadings, "/log/<string:StationID>/<float:Sensor1>/<float:Sensor2>/<float:Sensor3>/<int:Verbose>")
+
+if __name__ == "__main__":
+
+    app.run(host="192.168.2.100", port="5000", debug=True)
+
